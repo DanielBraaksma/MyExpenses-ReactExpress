a7d8c8fc553494ce2dba38c5622728ff13abd9d1
Daniel Braaksma <danebraaksma@hotmail.com>
2023-08-12 16:19:11 -0400

    remove picture

--- src/features/components/Header.tsx
+++ src/features/components/Header.tsx
@@ -1,17 +1,15 @@
-import { Avatar } from '@mui/material';
-import '../../scss/Header.scss'
-import {MenuListComposition} from './MenuListComposition';
-
+import { Avatar } from "@mui/material";
+import "../../scss/Header.scss";
+import { MenuListComposition } from "./MenuListComposition";
 
 export const Header = () => {
-
-    return (
-        <header>
-            <Avatar style={{marginLeft: '1.25em', width: 56, height: 56}} src="/linkedin1.jpg" />
-            <h1 style={{justifySelf: 'center'}}>My Expense Tracker</h1>
-            <div className='menu-container'>
-                <MenuListComposition className='menu'></MenuListComposition>
-            </div>
-        </header>
-      );
-}
+  return (
+    <header>
+      <Avatar style={{ marginLeft: "1.25em", width: 56, height: 56 }} />
+      <h1 style={{ justifySelf: "center" }}>My Expense Tracker</h1>
+      <div className="menu-container">
+        <MenuListComposition className="menu"></MenuListComposition>
+      </div>
+    </header>
+  );
+};
